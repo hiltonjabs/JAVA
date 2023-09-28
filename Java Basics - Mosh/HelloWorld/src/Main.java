eb67bf69604b0947d6ec172236dbdb590c3f7d78
hiltonjabs <hiltonjabs@hotmail.com>
2023-09-28 18:46:40 +0200

    Adding more notes

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -1,15 +1,14 @@
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-    //String[] args is a parenthesis () and in this case is a parameter
-    //The parameter s used to pass values to our program
+    // String[] args is a parenthesis () and in this case is a parameter
+    // The parameter s used to pass values to our program
         System.out.println("Hello world!");
-    //every statement is terminated by a ";"
-    //void is a return type value and in this case means we are not expecting
-    //System is a system class in JAVA
-    //Inside this System class there are members so by typing System. we will see the different members(method) define
-    //in the system class
-    //out is the field and println is a method...
+    // every statement is terminated by a ";"
+    // void is a return type value and in this case means we are not expecting
+    // System is a system class in JAVA
+    // Inside this System class there are members so by typing System. we will see the different members(method) define
+    // in the system class
+    // out is the field and println is a method...
     }
 }
