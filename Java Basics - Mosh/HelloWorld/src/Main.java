c4a531ca8bb169aee93b70b90a265ecacf34d7a4
hiltonjabs <hiltonjabs@hotmail.com>
2023-09-28 23:42:24 +0200

    Adding code

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -1,11 +1,13 @@
 import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        //c:\Windows\...
-        String message = "c:\\Windows\\...";
-        System.out.println(message);
-
-
+        int [] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 5;
+        numbers[2] = 4;
+        System.out.println(Arrays.toString(numbers));
     }
 }
