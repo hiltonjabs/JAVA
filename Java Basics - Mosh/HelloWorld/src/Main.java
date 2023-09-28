9e2d3c49f25e4d9c981ecca81d1a12e5f7fb7b44
hiltonjabs <hiltonjabs@hotmail.com>
2023-09-29 01:19:51 +0200

    Adding notes

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -1,13 +1,11 @@
+import com.sun.security.jgss.GSSUtil;
+
 import java.awt.*;
 import java.util.Arrays;
-import java.util.Date;
+
 
 public class Main {
     public static void main(String[] args) {
-        int [] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 5;
-        numbers[2] = 4;
-        System.out.println(Arrays.toString(numbers));
+        final float PI = 3.14F;
     }
 }
