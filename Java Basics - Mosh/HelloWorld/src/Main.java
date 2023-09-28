c92eaf689e860de1e9d071e2e61b545df47f7709
hiltonjabs <hiltonjabs@hotmail.com>
2023-09-28 20:44:52 +0200

    Adding notes

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -1,8 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello world" + "!!";
-        System.out.println(message.indexOf("o"));
+        String message = "   Hello world" + "!!  ";
+        System.out.println(message.trim());
+        System.out.println(message);
 
     }
 }
