d1db2fce398294f8699c3882805d184be388fadc
hiltonjabs <hiltonjabs@hotmail.com>
2023-09-29 15:30:48 +0200

    Adding notes

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -6,8 +6,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        double result = (double)10 / (double)4;
-        System.out.println(result);
-
+        int x = 1;
+        // x = x + 2;
+        x += 2;
+        System.out.println(x);
     }
 }
