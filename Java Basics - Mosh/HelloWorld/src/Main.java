fbd0d15fc178eadd3d475c47969e0bfdc00249f3
hiltonjabs <hiltonjabs@hotmail.com>
2023-09-30 14:45:20 +0200

    Adding to the notes

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String x = "1";
+        String x = "1.1";
         int y = Integer.parseInt(x) + 2;
         System.out.println(y);
     }
