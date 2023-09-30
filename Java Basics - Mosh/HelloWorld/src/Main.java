c9539a6088da028d218e7de8069d8bb8dd665a33
hiltonjabs <hiltonjabs@hotmail.com>
2023-09-30 15:43:37 +0200

    Adding code and notes

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -6,8 +6,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String x = "1.1";
-        float y = Float.parseFloat(x) + 2;
-        System.out.println(y);
+        int result = (int) (Math.random() * 100);
+        System.out.println(result);
     }
 }
