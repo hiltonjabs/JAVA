52607c807579a0f12ece3792f736524af36280c1
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-01 23:11:38 +0200

    Finalising the program for Mortgage Calculator and adding notes

--- Java Basics - Mosh/Mortgage Calculator/src/Main.java
+++ Java Basics - Mosh/Mortgage Calculator/src/Main.java
@@ -1,3 +1,4 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +8,23 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal:");
+        System.out.print("Principal: ");
         int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_A_YEAR;
+
+
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_A_YEAR;
+
+        double mortgage = principal
+                    * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                    / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
