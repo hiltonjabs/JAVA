c5162cc74cad6820a009c3715188f5ed20493298
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-02 00:13:53 +0200

    adding notes and code

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Name:");
-        String name = scanner.nextLine().trim();
-        System.out.println("You are " + name);
+        boolean hasHighIncome = false;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+
     }
 }
