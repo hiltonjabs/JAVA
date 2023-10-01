4e7b2839d2a361a141e35e2db070e8155adedc2a
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-01 21:14:10 +0200

    Adding code to Mortgage Calculator Project

--- Java Basics - Mosh/Mortgage Calculator/src/Main.java
+++ Java Basics - Mosh/Mortgage Calculator/src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        final byte PERCENT = 100;
+        final byte MONTHS_IN_A_YEAR = 12;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal:");
+        int principal = scanner.nextInt();
     }
 }
