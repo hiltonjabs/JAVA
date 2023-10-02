5a883a690828fc457ab36ccc3f9f63d202785a48
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-02 22:40:27 +0200

    adding code

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean hasHighIncome = false;//this is the high income
-        boolean hasGoodCredit = false;//this is the good credit
+        boolean hasHighIncome = false;
+        boolean hasGoodCredit = false;
         boolean hasCriminalRecord = false;
         boolean isEligible = hasHighIncome || hasGoodCredit;
 
