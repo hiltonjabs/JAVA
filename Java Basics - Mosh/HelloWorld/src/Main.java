2aa91c01f11815cb230bda33ea2d09c8c000a57f
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-06 15:52:36 +0200

    Removed code

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world again?");
-        System.out.println("I am still at it");
         
     }
 }
