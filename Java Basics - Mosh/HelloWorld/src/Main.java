3ad55287aacc06974c5d5fba5d82b01163b5ef4c
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-06 15:46:12 +0200

    Removed code

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello world again?");
+        System.out.println("Hello world again?");
         System.out.println("I am still at it");
         System.out.println("Coding and coding");
         System.out.println("Let's do this ");
-        System.out.println("a week in!!!");
-        System.out.println("tomorrow I will be even better");
-        System.out.println("I will be the best at this JAVA programming");
-
+        System.out.println("A week in!!!");
+        System.out.println("Tomorrow I will be even better");
+        
     }
 }
