6e3195b2e7e0694b181dcb56813a7681a72808fc
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-09 22:40:36 +0200

    editing code

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -7,7 +7,5 @@ public class Main {
         System.out.println("Lets add some code");
         System.out.println();
         System.out.println();
-        System.out.println();
-        System.out.println();
     }
 }
