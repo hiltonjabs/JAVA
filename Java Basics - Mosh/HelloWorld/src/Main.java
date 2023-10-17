708409735d14198cbbf3760e5cdba949656f0d81
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-17 22:52:39 +0200

    adding code

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         System.out.println("Hello world again?");
         System.out.println("Hello World Again");
-        System.out.println();
+        System.out.print("Hello World");
+        System.out.println("Hello World Once More ");
     }
 }
