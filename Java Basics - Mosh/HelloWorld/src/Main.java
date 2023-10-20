58c578b9173f6a509f3b1f6847c5d144226aed18
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-20 23:23:49 +0200

    Adding notes

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -2,13 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
-        System.out.println("Hello World!!");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println(sout
-        );
+        
     }
 }
