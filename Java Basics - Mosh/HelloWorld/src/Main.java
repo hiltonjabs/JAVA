dfa51a556e1208bb8c050d8ea53d087553fde3bb
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-22 01:02:11 +0200

    adding code

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int income = 120_000;
-        boolean hasHighIncome = (income > 100_000);
+        String className = income > 100_000 ? "First" : "Economy";
     }
 }
