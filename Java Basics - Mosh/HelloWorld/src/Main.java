06cddce726dd48441e2982e8f8c19cdbe1b979e7
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-23 00:24:41 +0200

    Adding Code

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String    while (i > 0) {
-            System.out.println("Hello World " + i);
-            i--;
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("quit")) {
+            System.out.println("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
         }
     }
 }
