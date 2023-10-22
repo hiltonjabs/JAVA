dea04502ebdd4fbf777a7ff4e03252868d270b61
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-22 02:12:36 +0200

    FizzBuzz project amended

--- Java Basics - Mosh/FizzBuzz/src/Main.java
+++ Java Basics - Mosh/FizzBuzz/src/Main.java
@@ -5,10 +5,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Number: ");
         int number = scanner.nextInt();
-        if (number % 5 == 0 && number % 3 ==0)
-            System.out.println("FizzBuzz");
-        else if (number % 5 == 0)
-            System.out.println("Fizz");
+        if (number % 5 == 0) {
+            if (number % 3 == 0)
+                System.out.println("FizzBuzz");
+            else
+                System.out.println("Fizz");
+        }
         else if (number % 3 == 0)
             System.out.println("Buzz");
     }
