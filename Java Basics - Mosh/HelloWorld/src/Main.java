23ed47bc14e0e5d5e699447f3798049e64ace439
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-22 23:46:40 +0200

    Adding Code

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++)
-            System.out.println("Hello World");
+        for (int i = 5; i > 0; i--)
+            System.out.println("Hello World" + i);
     }
 }
