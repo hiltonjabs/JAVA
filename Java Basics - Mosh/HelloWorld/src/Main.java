94a567f9b61f3f656d1732f933572aa1a16ea65f
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-23 01:11:20 +0200

    Adding NotesCode

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -4,16 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = "";
-        while (!input.equals("quit")) {
+        while (true) {
             System.out.println("Input: ");
             input = scanner.next().toLowerCase();
-        }
-
-        do {
-            System.out.println("Input: ");
-            input = scanner.next().toLowerCase();
+            if (input.equals("quit"))
+                break;
             System.out.println(input);
-
-        } while (!input.equals("quit"));
+        }
     }
 }
