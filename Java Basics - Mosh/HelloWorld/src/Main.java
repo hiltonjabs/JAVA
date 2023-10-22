8b51b7e78d51ea77b26aaa110788642004812e5c
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-22 02:56:56 +0200

    Adding code and notes

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -2,18 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int role = 1;
-
-        switch (role) {
-            case 1:
-                System.out.println("You're and admin");
-                break;
-
-            case 2:
-                System.out.println("You're a moderator");
-                break;
-
-            default:
-        }
+        for (int i = 0; i < 5; i++)
+            System.out.println("Hello World");
     }
 }
