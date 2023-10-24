bf77c2ed55b81d9569f680ab9f0ec8e31fe7c789
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-24 18:08:12 +0200

    Adding Notes on Clean coding

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] fruits = {"Apple","Mango","Orange"};
-        for (String fruit : fruits)
-            System.out.println(fruit);
+
+    }
+    public static void greetUser(String name){
+        System.out.println("Hello " + name);
     }
 }
