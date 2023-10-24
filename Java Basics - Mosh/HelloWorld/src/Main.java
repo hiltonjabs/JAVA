f602b54326977e2211711c13ec5916e0db785253
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-24 18:31:38 +0200

    Adding Code for method

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        greetUser( "Njabulo");
     }
-    public static void greetUser(String name){
+    public static void greetUser(String name) {
         System.out.println("Hello " + name);
     }
 }
