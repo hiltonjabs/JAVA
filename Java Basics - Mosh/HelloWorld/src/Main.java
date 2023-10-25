c38123a17049e6db9a781dea8a18643b88951665
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-26 01:26:15 +0200

    Adding cODE

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text.toUpperCase());
     }
 
 }
