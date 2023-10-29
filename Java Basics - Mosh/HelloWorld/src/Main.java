35d0cff86d09fb42aa78a45afa7c07ff926a5ba2
hiltonjabs <hiltonjabs@hotmail.com>
2023-10-29 23:14:25 +0200

    Adding Code

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
        var employee = new Employee();
        employee.setBaseSalary(500_000);
-       employee.setHourlyRate(21);
+       employee.setHourlyRate(20);
        int wage = employee.calculateWage(11);
         System.out.println(wage);
     }
