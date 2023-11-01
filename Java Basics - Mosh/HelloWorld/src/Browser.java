1d045eff0afb6ca7dc53f7a56d9adad9772847a2
hiltonjabs <hiltonjabs@hotmail.com>
2023-11-01 20:42:32 +0200

    Adding Code

---
+++ Java Basics - Mosh/HelloWorld/src/Browser.java
@@ -0,0 +1,16 @@
+public class Browser {
+    public void navigate(String address) {
+        String ip = findIpAdress(address);
+        String html = sendHttpRequest(ip);
+        System.out.println(html);
+    }
+
+    private String sendHttpRequest(String ip) {
+        return "<html></html>";
+    }
+
+    pr String findIpAdress(String address){
+        return "127.0.0.1";
+    }
+
+}
--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-       var employee = new Employee();
-       employee.setBaseSalary(50_000);
-       employee.setHourlyRate(20);
-       int wage = employee.calculateWage(10);
-        System.out.println(wage);
+        var browser = new Browser();
+        browser.navigate();
     }
 
 }
