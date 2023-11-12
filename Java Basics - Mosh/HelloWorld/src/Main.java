e92e16f26dc74e6183d8f86dd8a403b9d336c7bb
hiltonjabs <hiltonjabs@hotmail.com>
2023-11-12 23:22:02 +0200

    Adding Code

--- Java Basics - Mosh/HelloWorld/src/Main.java
+++ Java Basics - Mosh/HelloWorld/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        new Employee(50_000);
-        var employee = new Employee(50_000,20);
+        new Employee(60_000);
+        var employee = new Employee(60_000,20);
         int wage = employee.calculateWage(10);
         System.out.println(wage);
     }
