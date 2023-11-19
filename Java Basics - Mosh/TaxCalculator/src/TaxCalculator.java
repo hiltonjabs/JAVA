18f44cab3ee1cbcdb0cf3fe6e024a8e85ec25342
hiltonjabs <hiltonjabs@hotmail.com>
2023-11-19 23:37:13 +0200

    Adding Code

--- Java Basics - Mosh/TaxCalculator/src/TaxCalculator.java
+++ Java Basics - Mosh/TaxCalculator/src/TaxCalculator.java
@@ -11,7 +11,7 @@ public class TaxCalculator {
         double totalAmount = amount + taxAmount;
 
         System.out.println("The Original amount: $" + amount);
-        System.out.println("Tax (14%): $" + taxAmount);
+        System.out.println("Tax (14%): R" + taxAmount);
         System.out.println("Total amount after tax: $" + totalAmount);
 
         scanner.close();
